34162bf3d2533de6c8f7ec970d3bbbf939fe13ac
glesio.santos <glesioss@gmail.com>
2024-10-22 11:57:09 -0300

    Inicializando projeto de gestão de vendas dos projetos internos

---
+++ src/main/java/br/com/ohgestor/movenda/MovendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ohgestor.movenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ohgestor/movenda/MovendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ohgestor.movenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
